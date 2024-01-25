2e9180a0c4d5c125218f5fd6d4242ae67028064d
Mike Bramble <48955908+mikebramble@users.noreply.github.com>
2024-01-24 17:10:48 -0800

    Initial version

---
+++ script_py_load_amd_endmembers.py
@@ -0,0 +1,16 @@
+# Code written by: Michael Bramble | michael.s.bramble@jpl.nasa.gov
+# load unmixing endmembers for EMIT AMD investigation
+# 20240124 - initial version
+
+# load spectra
+
+
+
+# resample to EMIT wavelenths
+
+
+# set bad bands to -0.01
+
+
+
+# format endmembers into array

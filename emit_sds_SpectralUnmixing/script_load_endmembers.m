f2a53ea58d84d47580e5a6dfbe5fc7c516a1910d
Mike Bramble <48955908+mikebramble@users.noreply.github.com>
2024-02-16 11:09:33 -0800

    Removed SpectralUnmixing directory

--- emit_sds_SpectralUnmixing/script_load_endmembers.m
+++
@@ -1,9 +0,0 @@
-
-addpath(genpath('/Users/bramble/My Drive/_JPL_AMD/'))
-
-% load spectra
-A = importdata('basic_endmember_library.csv');
-wavelengths = A.data(1,:);
-spectra = A.data(2:end,:);
-% plot spectra
-plot(wavelengths,spectra)
